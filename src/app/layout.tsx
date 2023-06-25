ae9b833543dc3af696362c4b18c1995b4db1fbcc
CacaAlves00 <cacazinho99@gmail.com>
2023-06-25 14:27:46 -0300

    Creating homepage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import StyledComponentsRegistry from '../../lib/registry'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Plus_Jakarta_Sans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={plusJakartaSans.className}>
+        <StyledComponentsRegistry>{children}
+        </StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
